49c3a146bed1cf2ab919ed0f9d8a91e35e903eb7
NiGh10GaLe <ni10gl.com@gmail.com>
2020-04-09 17:22:29 +0200

    start app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white70,
+        appBar: AppBar(
+          title: Text('Magic 8 Ball Test'),
+          backgroundColor: Colors.black,
+        ),
+        body: Container(),
+      ),
+    ),
+  );
+}
