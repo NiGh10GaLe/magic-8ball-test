cb73a116158c2aac20e435015246435e53c1f32e
NiGh10GaLe <ni10gl.com@gmail.com>
2020-04-09 18:33:03 +0200

    Finishing the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,50 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white70,
-        appBar: AppBar(
-          title: Text('Magic 8 Ball Test'),
-          backgroundColor: Colors.black,
-        ),
-        body: Container(),
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
       ),
-    ),
-  );
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask Me Anuthing'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+            print(ballNumber);
+          },
+          child: Image(
+            image: AssetImage('images/ball$ballNumber.png'),
+          ),
+        ),
+      ),
+    );
+  }
 }
